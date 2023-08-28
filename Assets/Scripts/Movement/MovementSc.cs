5c96d7d572d27b1bd63ae8f97e6b62b58a275169
CosmicEntropyislife <64074169+CosmicMinority@users.noreply.github.com>
2023-08-28 13:19:17 +0200

    Scene folder organisation
    3D_asset
    2D_assets
    Scene
    Scripts

---
+++ Assets/Scripts/Movement/MovementSc.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSc : MonoBehaviour
+{
+    public static float speed = 20;
+    public static float thrust = 17;
+    public Rigidbody rb;
+
+    public float cooldown = 5;
+   
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Ver = Input.GetAxis("Vertical");
+        float Hor = Input.GetAxis("Horizontal");
+        Vector3 playermovement = new Vector3(Hor, 0f, Ver) * speed * Time.deltaTime;
+        transform.Translate(playermovement, Space.Self);
+
+      
+    }
+
+   
+}
